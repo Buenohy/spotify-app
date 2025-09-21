16b8f9c616f1d51ae29bde6a24c3b5967ebc36a9
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-09-21 06:51:34 -0300

    fix: error image spotify logo

--- components/ui/HeaderHome.tsx
+++ components/ui/HeaderHome.tsx
@@ -6,9 +6,13 @@ import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 export default function HeaderHome() {
   return (
     <View className="flex-row items-center justify-between p-7">
-      <Feather name="search" size={36} color="white" />
-      <Image source={require('../../assets/images/spotify-logo.png')} className="h-8 w-24" />
-      <SimpleLineIcons name="options-vertical" size={24} color="white" />
+      <Feather name="search" size={26} color="white" />
+      <Image
+        source={require('../../assets/images/spotify-logo.png')}
+        className="h-8 w-24"
+        resizeMode="contain"
+      />
+      <SimpleLineIcons name="options-vertical" size={22} color="white" />
     </View>
   );
 }
